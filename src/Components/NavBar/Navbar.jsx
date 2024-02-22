9e850ee5d9602017fd35ea837e8139f9fd928002
fahadkabali <kabalifahad@gmail.com>
2024-02-22 18:08:24 +0300

    update

--- src/Components/NavBar/Navbar.jsx
+++ src/Components/NavBar/Navbar.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useContext, useRef, useState } from 'react'
 import './Navbar.css'
 import logo from '../Assests/logo.png'
 import cart_icon from '../Assests/cart_icon.png'
 import { Link } from 'react-router-dom'
+import { ShopContext } from '../../Context/ShopContext'
+import nav_dropdown from '../Assests/nav_dropdown.png'
 
 const Navbar = () => {
     const [menu, setMenu] =useState("shop")
+    const {getTotalCartItems } = useContext(ShopContext)
+    const menuRef =useRef()
+
+    const dropdown_toggle =(e) =>{
+        menuRef.current.classList.toggle('nav-menu-visible')
+        e.target.classList.toggle('open')
+
+    }
   return (
     <div className='navbar'>
         <div className="nav-logo">
@@ -13,7 +23,8 @@ const Navbar = () => {
             <p>MY STORE</p>
 
         </div>
-        <ul className="nav-menu">
+        <img className='nav-dropdown' onClick={dropdown_toggle} src={nav_dropdown} alt=""/>
+        <ul ref={menuRef} className="nav-menu">
             <li onClick={()=>{setMenu("shop")}}><Link to='/' style={{textDecoration:'none'}}>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("mens")}}><Link to ='/mens' style={{textDecoration:'none'}}>Men</Link>{menu==="mens"?<hr/>:<></>}</li>
             <li onClick={()=>{setMenu("womens")}}><Link to='/womens'style={{textDecoration:'none'}}>Women</Link>{menu==="womens"?<hr/>:<></>}</li>
@@ -22,9 +33,7 @@ const Navbar = () => {
         <div className="nav-login-cart">
             <Link to='/login'><button>Login</button></Link>
             <Link to='/cart'><img src={cart_icon} alt="cartIcon"/></Link>
-            <div className="nav-cart-count">
-                0
-            </div>
+            <div className="nav-cart-count">{getTotalCartItems()}</div>
         </div>
       
     </div>
